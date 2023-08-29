240ea09d142378777760d092e9d900507e738788
pedrogalvao <pedroleitegalvao@gmail.com>
2023-08-29 11:49:43 +0200

    Force= g + ((10 - Position) - 2* Current_Speed)

--- src/drone_control.adb
+++ src/drone_control.adb
@@ -7,23 +7,45 @@ function HW_CB (Request : AWS.Status.Data) return AWS.Response.Data is
     Position_X : Float;
     Position_Y : Float;
     Position_Z : Float;
+    Current_Postition : Vec3;
+    Current_Speed : Vec3;
+    Time_Interval : Float;
+    Second_Last_Index : Natural;
+    Force_Y : Float;
 begin
     
     Position_X := Float'Value (AWS.Parameters.Get (AWS.Status.Parameters (Request), "position_x"));
     Position_Y := Float'Value (AWS.Parameters.Get (AWS.Status.Parameters (Request), "position_y"));
     Position_Z := Float'Value (AWS.Parameters.Get (AWS.Status.Parameters (Request), "position_z"));
 
-    Put_Line ("Height: " & AWS.Parameters.Get (AWS.Status.Parameters (Request), "position_y"));
+    timestamps.append(Clock);
 
-    if Position_Y > 11.0 then
-        return AWS.Response.Build ("text/html", "{""force"":[0,9.5,0]}");
-    elsif Position_Y > 10.0 then
-        return AWS.Response.Build ("text/html", "{""force"":[0,9.7,0]}");
-    elsif Position_Y > 9.0 then
-        return AWS.Response.Build ("text/html", "{""force"":[0,9.85,0]}");
+    Current_Postition := (X=>Position_X, Y=>Position_Y, Z=>Position_Z);
+
+    if timestamps.Last_Index > 1 then 
+        Second_Last_Index := Natural(Integer(timestamps.Last_Index) - 1);
+        Time_Interval := Float(timestamps.Last_Element - timestamps(Second_Last_Index));
+        Current_Speed := (
+                            X => (Position_X - positions.Last_Element.X) / Time_Interval, 
+                            Y => (Position_Y - positions.Last_Element.Y) / Time_Interval, 
+                            Z => (Position_Z - positions.Last_Element.Z) / Time_Interval
+                        );
     else
-        return AWS.Response.Build ("text/json", "{""force"":[0,10.0,0]}");
+        Current_Speed := (
+                            X => 0.0,
+                            Y => 0.0,
+                            Z => 0.0
+                        );
     end if;
+
+    positions.Append(Current_Postition);
+    speeds.Append(Current_Speed);
+
+    Put_Line ("Height: " & AWS.Parameters.Get (AWS.Status.Parameters (Request), "position_y"));
+    Force_Y := 9.81 + ((10.0 - Position_Y) - 2.0 * Current_Speed.Y);
+    Put_Line("Force_Y: " & Float'Image(Force_Y));
+
+    return AWS.Response.Build ("text/json", "{ ""force"" :[0, " & Float'Image(Force_Y) & ", 0]}");
 end HW_CB;
 
 end Drone_Control;
--- src/drone_control.ads
+++ src/drone_control.ads
@@ -2,8 +2,35 @@ with AWS.Response;
 with AWS.Status;
 with AWS.Server;
 with AWS.Parameters;
+with Ada.Calendar; use Ada.Calendar;
+with Ada.Containers.Vectors;
+with Ada.Containers; use Ada.Containers;
 
 package Drone_Control is
+
+   type Vec3 is record 
+      X : Float;
+      Y : Float;
+      Z : Float;
+   end record;
+
+   
+   package Vec3Vector is new
+     Ada.Containers.Vectors
+       (Index_Type   => Natural,
+        Element_Type => Vec3);
+
+   package TimeVector is new
+     Ada.Containers.Vectors
+       (Index_Type   => Natural,
+        Element_Type => Time);
+      
+   timestamps : TimeVector.Vector;
+   positions : Vec3Vector.Vector;
+   speeds : Vec3Vector.Vector;
+   curr_time : Time := Clock;
+   speed : Vec3 := (X => 0.0, Y=> 0.0, Z => 0.0);
+   position : Vec3 := (X => 0.0, Y=> 0.0, Z => 0.0);
 
    function HW_CB (Request : AWS.Status.Data) return AWS.Response.Data;
 
